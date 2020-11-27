682a1a032550c7df7df31301ac885bd57eaf5374
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-11-28 01:10:04 +0300

    task done

--- src/js/app.js
+++ src/js/app.js
@@ -33,57 +33,53 @@ const films = JSON.parse(`
   ]
 `);
 
-function fillFilmElem(filmElem) {
+function createFilmElem(filmObj) {
+  const filmTr = document.createElement('tr');
   const filmParams = document.querySelector('.films').tHead.firstElementChild.children;
   for (const param of filmParams) {
     const paramTd = document.createElement('td');
     switch (param.textContent) {
       case 'year':
-        paramTd.textContent = `(${filmElem.dataset[param.textContent]})`;
+        paramTd.textContent = `(${filmObj[param.textContent]})`;
         break;
       case 'imdb':
-        paramTd.textContent = `imdb: ${Number(filmElem.dataset[param.textContent]).toFixed(2)}`;
+        paramTd.textContent = `imdb: ${Number(filmObj[param.textContent]).toFixed(2)}`;
         break;
       default:
-        paramTd.textContent = filmElem.dataset[param.textContent];
+        paramTd.textContent = filmObj[param.textContent];
     }
-    filmElem.append(paramTd);
+    filmTr.append(paramTd);
   }
+  return filmTr;
 }
 
 function renderFilms(filmData) {
-  const filmTable = document.querySelector('.films').tBodies[0];
-  for (const film of filmData) {
-    const filmElem = document.createElement('tr');
-    // eslint-disable-next-line guard-for-in
-    for (const filmParam in film) {
-      filmElem.dataset[filmParam] = film[filmParam];
-    }
-    fillFilmElem(filmElem);
-    filmTable.append(filmElem);
+  const filmsTBody = document.querySelector('.films').tBodies[0];
+  filmsTBody.innerHTML = '';
+  for (const filmObj of filmData) {
+    filmsTBody.append(createFilmElem(filmObj));
   }
 }
 
-function sortFilms(attribute, descending = false) {
+function sortFilms(filmData, attribute, descending = false) {
   const headColumns = document.querySelector('.films thead tr').children;
   for (const column of headColumns) {
     column.innerText = column.innerText.replace(/\s.*/, '');
   }
 
-  const filmsCollection = document.querySelector('.films').tBodies[0].children;
-  const filmsArr = Array.from(filmsCollection);
+  const filmsArr = filmData.slice();
 
   if (attribute === 'title') {
     filmsArr.sort((a, b) => {
-      if (a.dataset[attribute] < b.dataset[attribute]) return -1;
-      if (a.dataset[attribute] > b.dataset[attribute]) return 1;
+      if (a[attribute] < b[attribute]) return -1;
+      if (a[attribute] > b[attribute]) return 1;
       return 1;
     });
   }
   if (attribute === 'id' || attribute === 'year' || attribute === 'imdb') {
     filmsArr.sort((a, b) => {
-      if (+a.dataset[attribute] < +b.dataset[attribute]) return -1;
-      if (+a.dataset[attribute] === +b.dataset[attribute]) return 0;
+      if (+a[attribute] < +b[attribute]) return -1;
+      if (+a[attribute] === +b[attribute]) return 0;
       return 1;
     });
   }
@@ -92,11 +88,7 @@ function sortFilms(attribute, descending = false) {
     filmsArr.reverse();
   }
 
-  for (let i = 0; i < filmsArr.length; i += 1) {
-    if (filmsArr[i] !== filmsCollection[i]) {
-      filmsCollection[i].before(filmsArr[i]);
-    }
-  }
+  renderFilms(filmsArr);
 
   const sortColumn = document.getElementById(`film-${attribute}`);
   if (descending) {
@@ -107,14 +99,14 @@ function sortFilms(attribute, descending = false) {
 }
 
 const sortAttributes = [
-  ['id'],
-  ['id', true],
-  ['title'],
-  ['title', true],
-  ['year'],
-  ['year', true],
-  ['imdb'],
-  ['imdb', true],
+  [films, 'id'],
+  [films, 'id', true],
+  [films, 'title'],
+  [films, 'title', true],
+  [films, 'year'],
+  [films, 'year', true],
+  [films, 'imdb'],
+  [films, 'imdb', true],
 ];
 
 renderFilms(films);
